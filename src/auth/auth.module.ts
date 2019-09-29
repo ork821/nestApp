f3e4e101998069a2e613ef1a9719160f7d54a56c
ork821 <ork821@mail.ru>
2019-09-29 19:54:05 +0300

    jwt and passport added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import {AuthController} from './auth.controller';
 import {AuthService} from './auth.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {UserRepository} from './user.repository';
+import {JwtModule} from '@nestjs/jwt';
+import {PassportModule} from '@nestjs/passport';
 
 @Module({
     imports: [
+        PassportModule.register({defaultStrategy: 'jwt'}),
+        JwtModule.register({
+            secret: 'OrkSecret',
+            signOptions: {
+                expiresIn: 60 * 60 * 5, // 5 hours
+            },
+        }),
         TypeOrmModule.forFeature([UserRepository]),
     ],
     controllers: [AuthController],
